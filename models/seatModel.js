06425989cb1ae579504eb488e4a1af8eb6f8c654
Sharanya14parol <156458949+Sharanya14parol@users.noreply.github.com>
2024-12-19 11:33:21 +0530

    first working project

--- models/seatModel.js
+++ models/seatModel.js
@@ -1,10 +1,10 @@
-const db = require('../config/db');
+const db = require("../config/db");
 
 // Insert seat allocations into the database
 const insertSeatAllocations = (seatAllocations, callback) => {
-    const query = 'INSERT INTO SeatAllocation (student_id, seat_type, seat_number) VALUES ?';
-    db.query(query, [seatAllocations], callback);
+  const query =
+    "INSERT INTO SeatAllocation (student_id, seat_type, seat_number, classroom_number) VALUES ?";
+  db.query(query, [seatAllocations], callback);
 };
 
 module.exports = { insertSeatAllocations };
-
